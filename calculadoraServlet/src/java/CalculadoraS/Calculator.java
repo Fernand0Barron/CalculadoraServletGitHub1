afac36a019f678e7ba835e64a910d12fa751dc5a
Barron Ramirez Fernando Isahit <>
2018-09-06 08:35:12 -0500

    commit inicial @emmanuel789

--- calculadoraServlet/src/java/CalculadoraS/Calculator.java
+++ calculadoraServlet/src/java/CalculadoraS/Calculator.java
@@ -9,62 +9,67 @@ public   class   Calculator  extends   HttpServlet
 public   void   doPost(HttpServletRequest   req,   HttpServletResponse res)
 throws   ServletException,   IOException
     {
-    double   num1,   num2,   result;
+    double   numA,   numB,   result;
     int   operacion;
 
     ServletOutputStream   out   =   res.getOutputStream();
 
-    num1   =   Double.parseDouble(req.getParameter("num1"));
-    num2   =   Double.parseDouble(req.getParameter("num2"));
+    numA   =   Double.parseDouble(req.getParameter("numA"));
+    numB   =   Double.parseDouble(req.getParameter("numB"));
     operacion = Integer.parseInt(req.getParameter("operacion"));
 
-    result   =   calcularRes(num1,num2,operacion);
+    result   =   calcularRes(numA,numB,operacion);
 
     out.println("<html>");
     out.println("<head><title>.:CalculadoraServlet:.</title></head>");
+    out.println("<style>");
+    out.println("body {\n" +
+"    background-color: beige;\n" +
+"}");
+    out.println("</style>");
     out.println("<body>");
-    out.println("<h1><center>El resultado es</center></h1>");
+    out.println("<h1><center> \"El resultado es\"  </center></h1>");
     out.println("<h2>   <b><center>"+  result   +"</center></b></h2>");
     out.println("</body>");
     out.println("</html>");
     out.close();
     }
 
-    public   double   calcularRes(double   num1,   double   num2,   int   operacion)
+    public   double   calcularRes(double   numA,   double   numB,   int   operacion)
     {
     double   result   =   0;
 
         switch   (operacion)
         {
         case   1:
-            return   num1   +   num2;
+            return   numA   +   numB;
             
         case   2:
-            return   num1   -   num2;
+            return   numA   -   numB;
             
         case   3:
-            return   num1   *   num2;
+            return   numA   *   numB;
             
         case   4:
-            return   num1   /   num2;
+            return   numA   /   numB;
             
         case 5:
-            return  Math.acos(num1);
+            return  Math.acos(numA);
             
         case 6:
-            return  Math.cos(num1);
+            return  Math.cos(numA);
             
         case 7:
-            return  Math.asin(num1);
+            return  Math.asin(numA);
             
         case 8:
-            return  Math.sin(num1);
+            return  Math.sin(numA);
             
         case 9:
-            return   Math.log10(num1);
+            return   Math.log10(numA);
         
         case 10:
-            return  Math.abs(num1);
+            return  Math.abs(numA);
         }
 
     return   result;
